6ee56e126795e12b242a393e349a37faf8bb4023
Jong-Min <codejomo99@gmail.com>
2025-06-09 15:27:00 +0900

    git init

---
+++ src/test/java/com/sparta/authassignment/AuthAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.authassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
